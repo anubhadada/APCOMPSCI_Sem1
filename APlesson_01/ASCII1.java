e042c7ae107786364016f5acd090be2cf95a2e5d
anubhadada <anu.bhadada@gmail.com>
2016-09-02 11:58:37 -0700

    changed file

--- APlesson_01/ASCII1.java
+++ APlesson_01/ASCII1.java
@@ -2,7 +2,10 @@ public class ASCII1
 {
 	public static void main(String [] args)
 	{
-		System.out.println("\|\___/|\");
-		System.out.println("\/ @ @  \\")
+		System.out.println("H    H  EEEEEE  L      L       000       W   W   000   RRRR   L       DDDD   ! !");
+		System.out.println("H    H  E       L      L      0   0      W W W  0   0  R   R  L       D   D  ! !");
+		System.out.println("HHHHHH  EEEEEE  L      L      0   0      W W W  0   0  RRRR   L       D   D  ! !");
+		System.out.println("H    H  E       L      L      0   0       W W   0   0  R   R  L       D   D  ");
+		System.out.println("H    H  EEEEEE  LLLLLL LLLLLL  000        W W    000   R   R  LLLLLL  DDDD   ! !");
 	}
 }
