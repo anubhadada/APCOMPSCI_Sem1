fa3ae428c822df9aec49ea0836934fc63af17689
anubhadada <anu.bhadada@gmail.com>
2016-09-02 12:13:27 -0700

    added file

---
+++ APlesson_01/ASCII2.java
@@ -0,0 +1,12 @@
+public class ASCII2
+{
+	public static void main(String [] args)
+	{
+		System.out.println("  |\\ __ /|");
+		System.out.println(" /   @   @  \\");
+		System.out.println("(  >   o  <  )");
+		System.out.println("\\     -      /");
+		System.out.println(" \\  >>X<<  /");
+		System.out.println("  /    0    \\");
+	}
+}
