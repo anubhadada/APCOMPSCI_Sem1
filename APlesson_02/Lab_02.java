c74c14506ae7bafb4878273e6308ba401f2f0b0f
anubhadada <anu.bhadada@gmail.com>
2016-09-07 11:48:39 -0700

    added files

---
+++ APlesson_02/Lab_02.java
@@ -0,0 +1,25 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 5;
+		int b = 10;
+		int mult = a * b;
+		
+		System.out.println("5 multiplied by 10 is " + mult);
+	
+		String name = "Anu Bhadada ";
+		String address = "13531 Candy Tuft Path ";
+		String city = "San Diego, CA";
+		String zip = "-92130";
+		
+		System.out.println(name + address + city + zip);
+		
+		int l = 5;
+		int w = 3;
+		int h = 6;
+		int surfaceArea = 2 * (w * l + h * l + h * w);
+		
+		System.out.println("The surface area of your rectangular prism is " + surfaceArea);
+	}
+}
