62b4b2485b41a353f07247efd8b5f7ac27cdfef9
MrRobinette <richard.robinette@sduhsd.net>
2016-09-28 01:36:56 -0700

    Updated

--- APlesson_03/BMI.java
+++ APlesson_03/BMI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class BMI
@@ -19,30 +18,5 @@ public class BMI
 		
 		System.out.println("Your BMI is " + BMI);
 		
-	}
-	
-=======
-import java.util.Scanner;
-
-public class BMI
-{
-	public static void main(String[]args)
-	{
-		Scanner kb = new Scanner(System.in);
-		
-		System.out.println("What is your height?");
-		
-		double height = kb.nextDouble();
-		
-		System.out.println("What is your weight?");
-		
-		double weight = kb.nextDouble();
-		
-		double BMI = (weight / (height * height)) * 703;
-		
-		System.out.println("Your BMI is " + BMI);
-		
-	}
-	
->>>>>>> origin/master
+	}	
 }
